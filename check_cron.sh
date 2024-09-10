2e30bc86f27e9643a3c019cb3ee9ead08ec88df5
coco177938 <162773917+coco177938@users.noreply.github.com>
2024-09-10 20:14:01 +0800

    Update check_cron.sh

--- check_cron.sh
+++ check_cron.sh
@@ -7,7 +7,7 @@ CRON_NEZHA="./yixiu.sh >/dev/null 2>&1 &"
 
 echo "检查并添加 crontab 任务"
 
-  if [ -e "${WORKDIR}/start.sh" ]; then
+  if [ -e "${WORKDIR}" ]; then
     echo "添加 nezha 的 crontab 重启任务"
     (crontab -l | grep -F "@reboot pkill -kill -u $USER && ${CRON_NEZHA}") || (crontab -l; echo "@reboot pkill -kill -u $USER && ${CRON_NEZHA}") | crontab -
     (crontab -l | grep -F "* * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}") || (crontab -l; echo "*/12 * * * * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}") | crontab -
