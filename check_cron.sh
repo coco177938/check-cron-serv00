d8a0b412da2501576065262ba5cfe1a53bb8fc7b
coco177938 <162773917+coco177938@users.noreply.github.com>
2024-09-13 22:21:45 +0800

    Update check_cron.sh

--- check_cron.sh
+++ check_cron.sh
@@ -2,15 +2,15 @@
 
 USER=$(whoami)
 USER_LOWER="${USER,,}"
-WORKDIR="/home/${USER_LOWER}/yixiu.sh"
-CRON_NEZHA="./yixiu.sh >/dev/null 2>&1 &"
+WORKDIR="/home/${USER_LOWER}/xray/config.json"
+CRON_NEZHA="cd xray && nohup ./xray -c config.json >/dev/null 2>&1 &"
 
 echo "检查并添加 crontab 任务"
 
   if [ -e "${WORKDIR}" ]; then
     echo "添加 nezha 的 crontab 重启任务"
     (crontab -l | grep -F "@reboot pkill -kill -u $USER && ${CRON_NEZHA}") || (crontab -l; echo "@reboot pkill -kill -u $USER && ${CRON_NEZHA}") | crontab -
-    (crontab -l | grep -F "* * pgrep -x \"bot\" > /dev/null || ${CRON_NEZHA}") || (crontab -l; echo "*/12 * * * * pgrep -x \"bot\" > /dev/null || ${CRON_NEZHA}") | crontab -
+    (crontab -l | grep -F "* * pgrep -x \"xray\" > /dev/null || ${CRON_NEZHA}") || (crontab -l; echo "*/12 * * * * pgrep -x \"bot\" > /dev/null || ${CRON_NEZHA}") | crontab -
   
 fi
 
